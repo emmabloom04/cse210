54840fa365e98325beb358e5cefa65b664810c01
emmabloom04 <blo23006@byui.edu>
2024-06-11 21:22:58 -0600

    Finishing prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        Console.WriteLine(number);
+        
+        Console.WriteLine("Welcome to the number game! You will be guessing a random number from 1 to 100.");
+
+        int userGuess;
+
+        do 
+        {
+            Console.Write("Guess: ");
+            string guess = Console.ReadLine();
+            userGuess = int.Parse(guess);
+            if (userGuess > 100 || userGuess < 0)
+            {
+                Console.WriteLine("That number is out of the range.");
+            }
+            else if (number > userGuess)
+            {
+                Console.WriteLine($"The number is higher than {userGuess}.");
+            }
+            else if (number < userGuess)
+            {
+                Console.WriteLine($"The number is lower than {userGuess}.");
+            }
+        } while (userGuess != number);
+
+        Console.WriteLine("Great job! You guessed it!");
     }
 }
