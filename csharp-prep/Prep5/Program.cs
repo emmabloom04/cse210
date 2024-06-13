794bb1fb89042adbbfd2e55f3c326c576a99c6b1
emmabloom04 <blo23006@byui.edu>
2024-06-13 14:50:09 -0600

    Finishing prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,43 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int favoriteNumber)
+    {
+        int square = favoriteNumber * favoriteNumber;
+        return square;
+    }
+
+    static void DisplayResult(string userName, int favoriteNumber)
+    {
+        int square = SquareNumber(favoriteNumber);
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int favoriteNumber = PromptUserNumber();
+        DisplayResult(userName, favoriteNumber);
     }
 }
