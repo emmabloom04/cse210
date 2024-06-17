45e271c7d7c3900d6f64d65a18218865e958cf72
emmabloom04 <blo23006@byui.edu>
2024-06-17 14:49:24 -0600

    adding another instance of the circle class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,8 +8,13 @@ class Program
     static void Main(string[] args) // you need to have a static main func for the program to work
     {
         Console.WriteLine("Hello Sandbox World!");
+
         Circle myCircle = new Circle(10);
         Console.WriteLine($"{myCircle.GetArea()}");
+
+        Circle unitCircle = new Circle(1);
+        Console.WriteLine($"{unitCircle.GetCircumference()}");
+        Console.WriteLine($"{unitCircle.GetDiameter()}");
 
     }
 }
