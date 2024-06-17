b5083562098cfc6ddb1d550972799c26b12dd583
emmabloom04 <blo23006@byui.edu>
2024-06-17 16:57:14 -0600

    making the job class (pre testing)

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,41 @@
+using System.Runtime.InteropServices.Marshalling;
+
+class Job 
+{
+    private string _comapny;
+    private string _jobTitle;
+    private string _startYear;
+    private string _endYear;
+
+    public Job(string company, string jobTitle, string startYear, string endYear)
+    {
+        company = _comapny;
+        jobTitle = _jobTitle;
+        startYear = _startYear;
+        endYear = _endYear;
+    }
+
+    public string GetCompany()
+    {
+        Console.Write("Enter your company: ");
+        return _comapny = Console.ReadLine();
+    }
+
+    public string GetJobTitle()
+    {
+        Console.Write("Enter your job title: ");
+        return _jobTitle = Console.ReadLine();
+    }
+
+    public string GetStartYear()
+    {
+        Console.Write("Enter your start year: ");
+        return _startYear = Console.ReadLine();
+    }
+
+    public string GetEndYear()
+    {
+        Console.Write("Enter your end year: ");
+        return _endYear = Console.ReadLine();
+    }
+}
