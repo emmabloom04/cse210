0eb61dc9a8f0350e0271e8e2b99c3edc7bbfa01d
emmabloom04 <blo23006@byui.edu>
2024-06-24 19:34:10 -0600

    Starting scripture memorization program

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Console.WriteLine("Welcome to the scripture mastery program!");
+
     }
 }
