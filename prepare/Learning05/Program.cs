411cc1e77eb90e18b42bd4bebce22e3ba69100e2
emmabloom04 <blo23006@byui.edu>
2024-07-08 13:35:04 -0600

    Finishing shape assignment

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("purple", 4));
+        shapes.Add(new Rectangle("red", 5, 6));
+        shapes.Add(new Circle("yellow", 10));
+
+        foreach (Shape shape in shapes) {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        }
+
     }
 }
