6357c143da09e2c1164d2acef54a2b0b8b45eb43
emmabloom04 <blo23006@byui.edu>
2024-07-11 00:13:08 -0600

    making goals class

--- prove/Develop05/Goals.cs
+++ prove/Develop05/Goals.cs
@@ -1,3 +1,27 @@
 class Goals {
-    
+    List<Goal> _goals = new List<Goal>();
+    Goal _goal;
+    int _totalPoints;
+    int _points;
+    public Goals(List<Goal> goals, Goal goal, int totalPoints, int points) {
+        _goals = goals;
+        _goal = goal;
+        _totalPoints = totalPoints;
+        _points = points;
+    }
+
+    public List<Goal> AddToList() {
+        _goals.Add(_goal);
+        return _goals;
+    }
+
+    public List<Goal> RemoveFromList() {
+        _goals.Remove(_goal);
+        return _goals;
+    }
+
+    public int AddPoints() {
+        _totalPoints += _points;
+        return _totalPoints;
+    }
 }
